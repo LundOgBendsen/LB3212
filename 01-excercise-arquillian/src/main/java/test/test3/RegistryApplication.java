808486a2459bc26426a472baac7637aece8c8375
Martin Boel <mabo@dibs.dk>
2016-10-27 10:42:23 +0200

    1st

---
+++ 01-excercise-arquillian/src/main/java/test/test3/RegistryApplication.java
@@ -0,0 +1,9 @@
+package test.test3;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("registry")
+public class RegistryApplication extends Application {
+
+}
